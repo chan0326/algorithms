d960967016e95f56c46506642c9f3a5d948d81cb
chan0326 <qkrdydcksz48@gmail.com>
2024-02-19 20:33:25 +0900

    240219-1 kaupMain

--- Dwarf.java
+++ Dwarf.java
@@ -6,19 +6,49 @@ public class Dwarf {
         Scanner sc = new Scanner(System.in);
         System.out.println("9개의 수가 주어질때 오름차순으로 7개를 나타내라");
         int[] arry1 = new int[9];
+        int sum = 0;
         for (int i = 0; i < arry1.length; i++) {
             arry1[i] = sc.nextInt();
-
+            sum += arry1[i];
         }
+        int c =0;
 
-        for (int i = 0; i < arry1.length-1; i++) {
-            if (arry1[i] > arry1[i+1]) {
-                arry1[i] = arry1[i + 1];
-                arry1[i + 1] = arry1[i];
+        for (int j = 0; j < 8; j++) {
+            for (int i = 1; i < 9; i++) {
+                if (sum - arry1[j] - arry1[i] == 100) {
+
+                    arry1[j] = 100;
+                    arry1[i] = 100;
+                    break;
+                }
+
+
+
+
+
+
             }
         }
-        System.out.println(Arrays.toString(arry1));
-
-
+        for (int a = 0; a < 8; a++) {
+            for (int b = 1; b < 9; b++) {
+                if (arry1[a] > arry1[b]) {
+                    arry1[c] = arry1[a];
+                    arry1[a] = arry1[b];
+                    arry1[b] = arry1[c];
+                }
+            }
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println(arry1[i]);
+        }
     }
+
+
 }
+
+
+
+
+
+
+
