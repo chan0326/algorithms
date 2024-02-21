003c26e17617a6f130373d20b9411caaae391ebe
chan0326 <qkrdydcksz48@gmail.com>
2024-02-21 21:13:57 +0900

    20204-02-20 forforinterface

--- matrixTest/Gugu.java
+++ matrixTest/Gugu.java
@@ -1,14 +1,33 @@
 package matrixTest;
 
 public class Gugu {
+
+
     public static void main(String[] args) {
-        int arry[][] = new int[8][9];
-        for (int i=2;i<10;i++){
-            for (int j=1;j<10;j++){
+        int arry[][] = new int[10][10];
+        for (int i = 2; i < arry.length; i++) {
+            for (int j = 1; j < arry.length; j++) {
+                arry[i][j] = i * j;
+
 
             }
 
+
         }
+
+        for (int i = 2; i < arry.length; i++) {
+            for (int j = 1; j < arry[i].length; j++) {
+                if (arry[i][j] >= 10) {
+                    System.out.printf("%d * %d =  %s  ", i, j, arry[i][j] + "");
+                } else {
+                    System.out.printf("%d * %d =  %s  ", i, j, arry[i][j] + " ");
+                }
+
+            }
+            System.out.println();
+        }
+
 
     }
 }
+
