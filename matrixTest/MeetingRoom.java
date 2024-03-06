83c53052f78dbba8e677b4a9c21a752d0197f9c4
chan0326 <qkrdydcksz48@gmail.com>
2024-03-06 19:42:29 +0900

    2024-03-06

--- matrixTest/MeetingRoom.java
+++ matrixTest/MeetingRoom.java
@@ -11,51 +11,65 @@ public class MeetingRoom {
         int N = sc.nextInt();
         int[][] array = new int[N][2];
         for (int i = 0; i < N; i++) {
-            array[i][0]= sc.nextInt();
-            array[i][1]= sc.nextInt();
+            array[i][0] = sc.nextInt(); //시작시간
+            array[i][1] = sc.nextInt(); //종료시간
         }
-        Arrays.sort(array, Comparator.comparingInt((int[] o) -> o[1]));
-        for (int i=0 ; i<N; i++){
-//            System.out.println(Arrays.toString(array[i]));
 
-        }
-        int[] temp;
+        Arrays.sort(array, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]){
+                    return o1[0] - o2[0];
+                }
+                else return o1[1] - o2[1];
+            }
+        });
+
+
+//      --구시대적 방법---
+//        int temp;
 //        for (int i = 0; i < N; i++) {
-//            for (int j = i+1; j < N; j++)
+//            for (int j = i; j < N; j++) {
 //                if (array[i][1] > array[j][1]) {
-//                    temp = array[i];
-//                    array[i] = array[j];
-//                    array[j] = temp;
-//                    System.out.println(Arrays.deepToString(array));
+//                    temp = array[i][0];
+//                    array[i][0] = array[j][0];
+//                    array[j][0] = temp;
+//                    temp = array[i][1];
+//                    array[i][1] = array[j][1];
+//                    array[j][1] = temp;
 //                }
+//            }
+//        }
+//
+//
+//        int c;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i + 1; j < array.length; j++) {
+//                if (array[i][1] == array[j][1]) {
+//                    if (array[i][0] > array[j][0]) {
+//                        c = array[i][0];
+//                        array[i][0] = array[j][0];
+//                        array[j][0] = c;
+//                        c = array[i][1];
+//                        array[i][1] = array[j][1];
+//                        array[j][1] = c;
+//                    }
+//                }
+//            }
 //        }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++)
-                if (array[i][1] == array[j][1]) {
-                    if (array[i][0] > array[j][0]) {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                        System.out.println(Arrays.deepToString(array));
-                    }
-                }
-        }
+        // ---종료시간 오름차순정렬완료----
 
-        // 종료시간 오름차순정렬완료
-        int count = 0;
-        int [][] choice = new int[1][2];
-        choice[0]= array[0];
-        for (int i=1; i<N;i++){
-                if (choice[0][1]<=array[i][0]) {
-                    count++;
-                    choice[0]= array[i];
-                    break;
-
-
-
+        int count = 1;
+        int choice;
+        choice = array[0][1];
+        for (int i = 1; i < N; i++) {
+            if (choice <= array[i][0]) {
+                choice = array[i][1];
+                count++;
             }
         }
+        System.out.println(count);
 
     }
 }
