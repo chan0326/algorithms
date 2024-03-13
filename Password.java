c3b4f148ad6cfe4a73b9d11768f2175da4540ce1
chan0326 <qkrdydcksz48@gmail.com>
2024-03-13 19:54:46 +0900

    2024-03-13

--- Password.java
+++ Password.java
@@ -5,13 +5,33 @@ import java.util.Scanner;
 public class Password {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        char [] allchars = new char[]{'o','e'};
         char [] chars  = new char[]{'a', 'i', 'o', 'u', 'e'};
-        ArrayList arrayList = new ArrayList<>();
 
-        arrayList.add(sc.next());
+        String str =(sc.next());
+        char [] b =str.toCharArray();
+        System.out.println(b);
 
-        for (int i =0;i<arrayList.size();i++){
+
+        for (char i:b){
+            if (i != 'a' && i!= 'i' && i!= 'o' && i!= 'u' && i!= 'e' ){
+                System.out.println("<"+str+"> is not acceptable.1.");
+                break;}
+
+            }
+//            if (b.length>=3 && b[i] == b[i] && b[1]==b[]){
+//                if (arrayList.get(i) == allchars && arrayList.get(i+1) == allchars){
+//                    System.out.println("<"+str+"> is acceptable.2.");
+//                    break;
+//                }else {System.out.println("<"+str+"> is not acceptable.3.");
+//                break;}
+//
+//            }else {System.out.println("<"+str+"> is acceptable.4.");}
 
         }
+
+
+
+
     }
 }
