c6a2409d258cf2212c4b314b7c5325f08dc4b979
chan0326 <qkrdydcksz48@gmail.com>
2024-03-13 18:14:14 +0900

    2024-03-13

---
+++ Password.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        char [] chars  = new char[]{'a', 'i', 'o', 'u', 'e'};
+        ArrayList arrayList = new ArrayList<>();
+
+        arrayList.add(sc.next());
+
+        for (int i =0;i<arrayList.size();i++){
+
+        }
+    }
+}
