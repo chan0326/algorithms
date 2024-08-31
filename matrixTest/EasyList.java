6b4c0a990df098b55896bcfa118b2b282dfe3688
chan0326 <qkrdydcksz48@gmail.com>
2024-08-31 22:08:15 +0900

    2024-08-31

---
+++ matrixTest/EasyList.java
@@ -0,0 +1,14 @@
+package matrixTest;
+
+import java.util.Scanner;
+
+public class EasyList {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String a = sc.next();
+            String b = sc.next();
+            String c =  a+b;
+            System.out.println(a+b);
+        }
+    }
+
